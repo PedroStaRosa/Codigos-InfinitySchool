319568af9913ec398aa0aa22489f196dafd58186
Pedro Santa Rosa <55983952+PedroStaRosa@users.noreply.github.com>
2024-10-29 08:39:55 -0300

    Create main.py

---
+++ Resolução_pedro/Desf_Produtos/main.py
@@ -0,0 +1,17 @@
+from func import *
+
+##==== ADD
+print( add_product("Biscoito", 3.99 , 10) )
+print( list_products() ) 
+
+##=== Remove
+print( remove_product("ARROZ") )
+print( list_products() )
+
+##=== UPDATE
+print( update_product("Biscoito", 99.00 , 30) )
+print( list_products() )
+
+##=== List Final List
+print("## Lista FINAL.")
+print( list_products() )
